danbroooks/fpinscala

Initial commit of project structure

src/main/scala/fpinscala/Main.scala
@@ -1,0 +1,5 @@
+package fpinscala
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}

danbroooks/scala-typelevel-programming

src/main/scala/typelevel/Main.scala
@@ -1,0 +1,5 @@
+package typelevel
+
+object Main extends App {
+  println("Run the tests with sbt test")
+}